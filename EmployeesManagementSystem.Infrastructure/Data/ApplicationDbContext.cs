0b80c30efc254675cecce702254a75ad3882df5f
Mousa Khaleel <mousakhaleel22@gmail.com>
2025-02-02 22:34:22 +0200

    Add project files.

---
+++ EmployeesManagementSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EmployeesManagementSystem.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesManagementSystem.Infrastructure.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+		public DbSet<Department> Departments { get; set; }
+		public DbSet<Position> Positions { get; set; }
+		public DbSet<Employee> Employees { get; set; }
+	}
+}
